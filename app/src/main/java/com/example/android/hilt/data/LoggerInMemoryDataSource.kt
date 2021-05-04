2aa3d06eff3aa460b752216d7ef81901d823ea42
Navid Rojiani <nrojiani@gmail.com>
2021-05-04 11:42:19 -0400

    9 - Qualifiers, multiple bindings

---
+++ app/src/main/java/com/example/android/hilt/data/LoggerInMemoryDataSource.kt
@@ -0,0 +1,27 @@
+package com.example.android.hilt.data
+
+import dagger.hilt.android.scopes.ActivityScoped
+import java.util.LinkedList
+import javax.inject.Inject
+
+@ActivityScoped
+class LoggerInMemoryDataSource @Inject constructor() : LoggerDataSource {
+
+    private val logs = LinkedList<Log>()
+
+    init {
+        android.util.Log.d("LoggerInMem", "init")
+    }
+
+    override fun addLog(msg: String) {
+        logs.addFirst(Log(msg, System.currentTimeMillis()))
+    }
+
+    override fun getAllLogs(callback: (List<Log>) -> Unit) {
+        callback(logs)
+    }
+
+    override fun removeLogs() {
+        logs.clear()
+    }
+}
